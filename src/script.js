3d85e158631229b8e36e7be614c4a0f3f6c74924
lea-bee <twigg.lt20@gmail.com>
2024-08-24 15:27:18 +0200

    updated time function

--- src/script.js
+++ src/script.js
@@ -31,11 +31,13 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
 
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   return `${day} ${hours}:${minutes}`;
 }
 
